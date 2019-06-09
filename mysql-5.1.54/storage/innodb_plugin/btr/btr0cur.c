9ee0c55c55bcb89c00f0e13fe6a19dff79d4be4e
徐华华 <huahua.xhh@192.168.124.13>
2019-06-09 16:43:13 +0800

    update btr0cur

--- mysql-5.1.54/storage/innodb_plugin/btr/btr0cur.c
+++ mysql-5.1.54/storage/innodb_plugin/btr/btr0cur.c
@@ -1135,8 +1135,7 @@ btr_cur_optimistic_insert(
 		Subtract one byte for the encoded heap_no in the
 		modification log. */
 		//计算该压缩page的最大可用空闲空间
-		ulint	free_space_zip = page_zip_empty_size(
-			cursor->index->n_fields, zip_size) - 1;
+		ulint	free_space_zip = page_zip_empty_size(cursor->index->n_fields, zip_size) - 1;
 		ulint	n_uniq = dict_index_get_n_unique_in_tree(index);
 
 		ut_ad(dict_table_is_comp(index->table));
